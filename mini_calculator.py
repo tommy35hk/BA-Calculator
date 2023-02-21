d2b824a90c2f8309afdf0d6d2ddd01ed1babea99
tommytse <tommy35hk@gmail.com>
2023-02-21 15:49:34 +0800

    remove unused function

--- mini_calculator.py
+++ mini_calculator.py
@@ -11,33 +11,26 @@ def input_value():
     global final_reward
     items_cost.clear()
     try:
-        for items_list, entries in zip(items_list_collection, entries_collection):
+        for list, entries in zip(items_list_collection, entries_collection):
             item_cost = np.array([int(entry.get()) for entry in entries])
-            items_cost.append(sum(item_cost * items_list["Cost"].to_numpy()))
+            items_cost.append(sum(item_cost * list["Cost"].to_numpy()))
         bonus = np.array([float(entry.get()) for entry in bonus_entries])
     except ValueError:
         sys.exit("QTY should be positive integer")
     final_reward = np.ceil(basic_reward * (1 + bonus))
-    sol = minimize(lambda x: sum(x), x0, method='SLSQP', bounds=bnds,
+    sol = minimize(lambda x:sum(x), x0, method='SLSQP', bounds=bnds,
                    constraints=cons)
 
     for i, name in enumerate(quiz_name):
         tkinter.Label(resultFrame, text=name).grid(row=i, column=0)
         tkinter.Label(resultFrame, text=np.ceil(sol.x[i])).grid(row=i, column=1)
 
-
 def constraint1(x):
     return np.matmul(final_reward[0], x) - items_cost[0]
-
-
 def constraint2(x):
     return np.matmul(final_reward[1], x) - items_cost[1]
-
-
 def constraint3(x):
     return np.matmul(final_reward[2], x) - items_cost[2]
-
-
 def constraint4(x):
     return np.matmul(final_reward[3], x) - items_cost[3]
 
@@ -68,8 +61,8 @@ items_list3 = pd.DataFrame({
 })
 items_list4 = pd.DataFrame({
     "Name": ['Point', '現有'],
-    "Cost": [1, -1],
-    "pre_qty": [15000, 0]
+    "Cost": [1,-1],
+    "pre_qty":[15000,0]
 })
 items_list_collection = [items_list1, items_list2, items_list3, items_list4]
 entries_collection = []
@@ -81,16 +74,18 @@ basic_reward = np.array([
 ])
 final_reward = basic_reward.copy()
 bonus_entries = []
-items_cost = [0, 0, 0, 0]
-bnds = [(0, 1000) for _ in range(4)]
-x0 = np.array([1, 1, 1, 1])
-con1 = {'type': 'ineq', 'fun': constraint1}
-con2 = {'type': 'ineq', 'fun': constraint2}
-con3 = {'type': 'ineq', 'fun': constraint3}
-con4 = {'type': 'ineq', 'fun': constraint4}
+items_cost = [0,0,0,0]
+bnds = [(0,1000) for _ in range(4)]
+x0 = np.array([1,1,1,1])
+con1 = {'type':'ineq', 'fun':constraint1}
+con2 = {'type':'ineq', 'fun':constraint2}
+con3 = {'type':'ineq', 'fun':constraint3}
+con4 = {'type':'ineq', 'fun':constraint4}
 cons = [con1, con2, con3, con4]
 
 pd.set_option('display.unicode.east_asian_width', True)
+
+
 
 root = Tk()
 
@@ -120,6 +115,7 @@ for i, name in enumerate(items_name):
     my_entry.insert(0, 0)
     my_entry.grid(row=i, column=1)
     bonus_entries.append(my_entry)
+
 
 my_button = Button(botFrame, text="Input", command=input_value)
 my_button.pack()
